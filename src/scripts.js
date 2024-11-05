5739cd245abf1c7f09dd89975aae2ad9486e6fe8
PatriquiMarques <patriqui.marques@hotmail.com>
2024-11-05 20:55:17 -0300

    recuperação atividade avaliativa 2

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,11 @@
 alert("JAVASCRIPT CONECTADO COM HTML")
+const cadastroFuncionario = [];
+
+const nome = document.getElementById(nome);
+const  tecnlogias = document.getElementById(tecnlogias);
+const biografia = document.getElementById(biografia);
+const imagem  = document.getElementById(imagem);
+
+console.log(cadastroFuncionario)
+
+
